585f20f3511b1e3459137616dbb7bb9fcead00dc
tcasaniv <99104321+tcasaniv@users.noreply.github.com>
2026-02-10 23:19:37 -0500

    Habilita despliegue a GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,14 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
+    // const env = loadEnv(mode, '.', '');
     return {
       server: {
         port: 3000,
         host: '0.0.0.0',
       },
       plugins: [react()],
+       base: "/nicelims/",
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
